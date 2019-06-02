b5ff0bcfabb49cff83e7b2c7ea38f500aa2d7e76
Igor Chernyshov <ichernyshov@bk.ru>
2019-06-02 12:31:35 +0300

    Replaced pictures

---
+++ customCollectionViewLayout/PhotoCell.swift
@@ -0,0 +1,13 @@
+//
+//  SnowboardingPhotoCell.swift
+//  customCollectionViewLayout
+//
+//  Created by Igor Chernyshov on 02/06/2019.
+//  Copyright © 2019 Igor Chernyshov. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+}
